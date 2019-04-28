ee0ad3f2d0ea4be21d2ca4800964d37ca5bded02
NITESCU Cristian <cristian.nitescu@renault.com>
2019-04-28 11:33:37 +0200

    Fixed speed calculation

--- parts/speed.js
+++ parts/speed.js
@@ -9,19 +9,13 @@ class SpeedController {
     this.mpu6050.initialize();
     this.mpu6050.setFullScaleAccelRange(1);
     //
+    this.config.calibrationSamplesCount = 50;
+    this.config.speedThreshold = 5000;
+    //
     this.speed = 0;
-    this.calibrationValue = 0;
-    this.config.reduceFactor = 100;
-    this.config.filterValue = 5;
-    this.config.speedFilter = 2000;
-    this.sampleCount = 10;
-    this.sampleWeights = [];
-    this.sampleTotalWeights = 0;
-    for (let i = 0; i < this.sampleCount; i++) {
-      this.sampleWeights[i] = (i + 1)/this.sampleCount;
-      this.sampleTotalWeights += this.sampleWeights[i];
-    }
-    this.samples = [];
+    this.speedThresholdCount = 0;
+    this.calibrationValue = null;
+    this.calibrationSamples = [];
   }
   _addSampleGetAvg(s) {
 //    console.log('add', s);
@@ -33,40 +27,25 @@ class SpeedController {
   _readAcceleration() {
     this.mpu6050.getAcceleration((err, data) => {
       if (err) return console.error('error', err);
-//      console.log(data);
-      if (this.samples--) {
-        this.stream.write(`${new Date().getTime()};${data[0]},${data[1]},${data[2]}\n`);
-        console.log('sample ' + this.samples); 
-     } else {
-        console.log('ending');
-        this.stream.end();
-      }
-    });
-  }
-  __readAcceleration() {
-    const tt = new Date().getTime();
-//    console.log('time', tt - this.time);
-    this.time = tt;
-    this.mpu6050.getAcceleration((err, data) => {
-      if (err) return console.error('error', err);
-//      console.log('data', data[this.config.speedAxis]/this.reduceFactor);
-      const x = Math.round(this._addSampleGetAvg(Math.round(data[this.config.speedAxis]/this.config.reduceFactor)));
-//      console.log('samples', this.samples);
-      if (this.calibrationValue == null)
-        if (this.samples.length < this.sampleCount) return;
-        else {
-          this.calibrationValue = x;
-          console.log('calibration', this.calibrationValue);
-        }
-//      this.speed += (Math.round(data[this.config.speedAxis]/1000) - this.calibrationValue);
-      let value = x - this.calibrationValue;
-      if (Math.abs(value) < this.config.filterValue) value = 0;
-      if (value) {
-        this.speed += value;
-        console.log(this.speed);
+      //
+      if (this.config.calibrationSamplesCount > this.samples.length) {
+        this.samples.push(data[this.config.speedAxis]);
       } else {
-//        if (Math.abs(this.speed) < this.config.speedFilter) this.speed = 0;
+        if (this.calibrationValue == null) {
+          this.calibrationValue = this.samples.reduce((prev, sample) => { return prev + sample }, 0)/this.samples.length;
+        }
+        console.log('calibration', this.calibrationValue);
+        this.speed = this.speed + data[this.config.speedAxis] - this.calibrationValue;
+        if (this.speed < this.config.speedThreshold) {
+          this.speedThresholdCount++;
+          if (this.speedThresholdCount > this.config.calibrationSamplesCount) {
+            this.speed = 0;
+            this.speedThresholdCount = 0;
+          }
+        } else this.speedThresholdCount = 0;
       }
+      //
+      if (this.stream) this.stream.write(`${new Date().getTime()};${data[0]};${data[1]};${data[2]};${this.speed}\n`);
     });
   }
   _startAcquisition() {
@@ -80,11 +59,9 @@ class SpeedController {
   start() {
     this.time = new Date().getTime();
     this.stream = fs.createWriteStream(__dirname + '/' + new Date().toISOString());
-    this.samples = 10000;
     // calibrate
     this.mpu6050.getAcceleration((err, data) => {
-      this.calibrationValue = null; // Math.round(data[this.config.speedAxis]/this.config.reduceFactor);
-      console.log('calibration', this.calibrationValue);
+      this.calibrationValue = null;
       this._startAcquisition();
     });
   }
