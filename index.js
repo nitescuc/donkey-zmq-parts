b072eae064ab0df2e0990ee3a7ff75fddef88567
NITESCU Cristian <cristian.nitescu@renault.com>
2019-05-01 12:03:46 +0200

    Use reliable timer to publish

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const zmq = require('zmq')
 const publisher = zmq.socket('pub')
+const Gpio = require('pigpio').Gpio;
 
 const { SonarGroup } = require('./parts/hc-sr04');
 const { SpeedController } = require('./parts/speed');
 
+const PWM_SONAR = 21;
+const PWM_ACCEL = 22;
+const PWM_PUBLISH = 23;
+const SONAR_TRIGGER = 18;
+const SONAR_ECHO1 = 14;
+const SONAR_ECHO2 = 15;
+
 const sonarGroup = new SonarGroup({
-    triggerPin: 18,
-    pwmTimerPin: 21,
+    triggerPin: SONAR_TRIGGER,
+    pwmTimerPin: PWM_SONAR,
+    frequency: 100,
     sonars: [{
-        echoPin: 14
+        echoPin: SONAR_ECHO1
     }, {
-        echoPin: 15
+        echoPin: SONAR_ECHO2
     }]
 });
 sonarGroup.start();
 
 const speedController = new SpeedController({
-    pwmTimerPin: 22,
+    pwmTimerPin: PWM_ACCEL,
+    frequency: 400,
     speedAxis: 0
 });
 speedController.start();
@@ -28,8 +38,11 @@ publisher.bind('tcp://*:5555', function(err) {
         console.log('Listening on 5555')
 });
 
-setInterval(() => {
+const pwmTimer = new Gpio(PWM_PUBLISH, {mode: Gpio.OUTPUT, edge: Gpio.RISING_EDGE});
+pwmTimer.on('interrupt', (level, tick) => {
 	const distances = sonarGroup.read();
     publisher.send(['distance', ...distances]);
     publisher.send(['speed', speedController.getSpeed()]);
-}, 1000);
+});
+pwmTimer.pwmFrequency(40);
+pwmTimer.pwmWrite(128);
