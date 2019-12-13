684d8721f8f98453eac1e8b4ad3edf854fad1208
NITESCU Cristian <cristian.nitescu@renault.com>
2019-12-13 17:38:26 +0100

    Implement new zmq lib

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = require('config');
 const EventEmitter = require('events');
 const zmq = require('zeromq');
-const receiver = zmq.socket('sub');
+const receiver = new zmq.Subscriber;
 
 let conf;
 
@@ -11,7 +11,14 @@ class Config extends EventEmitter {
         this.overrides = {};
         receiver.connect(config.get('configServer.emitter'));
         receiver.subscribe('config');
-        receiver.on('message', (topic, message) => {
+        receiveMessages().catch(e => console.error('Error receiving configuration messages', e));
+    }
+    static getConfig() {
+        if (!conf) conf = new Config();
+        return conf;
+    }
+    async receiveMessages() {
+        for await (const [topic, message] of receiver) {
             console.log('Received new message', message.toString());
             try{
                 const newConf = JSON.parse(message.toString());
@@ -19,11 +26,7 @@ class Config extends EventEmitter {
             } catch(e) {
                 console.error(e);
             }
-        });
-    }
-    static getConfig() {
-        if (!conf) conf = new Config();
-        return conf;
+        }
     }
     get(key) {
         const value = this.overrides[key] || config.get(key);
